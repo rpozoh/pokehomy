f80f109b5daff9474337986033442f67457445d0
Mario Pozo <rpozoh@gmail.com>
2024-07-29 12:00:29 -0400

    versión

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
       home: Scaffold(
         body: GradientContainer(Alignment.topLeft, Alignment.bottomRight, [Colors.black, Colors.red]),
       ),
-      title: 'Randomizer by Rphisto v5.0.1',
+      title: 'Randomizer by Rphisto v5.0.2',
     ),
   );
 }
